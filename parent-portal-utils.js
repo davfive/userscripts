b06bf41dd4ba38d5bb78062be8a756e563a8f93f
David <davfive@gmail.com>
2020-05-07 18:26:47 -0700

    Update parent-portal-utils.js

--- parent-portal-utils.js
+++ parent-portal-utils.js
@@ -14,8 +14,8 @@ const ppu = window.ParentPortalUtils = {}
 
 ppu.hideCompleted = function() {
     // Hide the sidebar and move the main area to the left for printing
-    $('.sidebar').each(function(i,e) { alert($(this)); $(this).css('display', 'none') })
-    $('.inner-workspace-wrapper').each(function(i,e) { alert($(this)); $(this).css('padding-left', 0) })
+    $('.sidebar').each(function(i,e) { $(this).css('display', 'none') })
+    $('.inner-workspace-wrapper').each(function(i,e) { $(this).css('padding-left', 0) })
 
     // The main content is enclosed in an iframe so have to grab that first
     const iframe = $($("iframe")[0]).contents()[0]
